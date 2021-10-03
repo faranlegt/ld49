234766fa55798897639bcd9bac08e46e0d61c4a2
Yegor Zhumikov <yegor@yogurtthehor.se>
2021-10-03 23:11:30 +0300

    feat: band moving

---
+++ Assets/Scripts/Panel/Band.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Band : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+    public float maxPosition, minPosition, skipPosition, speed;
+
+    public float minOpacity = 0.3f, maxOpacity = 0.6f;
+
+    public void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Update()
+    {
+        var c = _renderer.color;
+        var p = transform.localPosition;
+
+        c.a = minOpacity + (maxOpacity - minOpacity) * ((maxPosition - p.y) / (maxPosition - minPosition));
+        _renderer.color = c;
+        
+        p.y -= speed * Time.deltaTime;
+
+        if (p.y < minPosition - skipPosition)
+        {
+            p.y = maxPosition;
+        }
+
+        transform.localPosition = p;
+    }
+}
